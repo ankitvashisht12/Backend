b6723558d5c128e24379a0f52ac32a8baf17b730
Dishebh Bhayana <43489708+Dishebh@users.noreply.github.com>
2020-09-07 20:26:28 +0530

    fix: authenticator issue (#65)

--- src/routes/v1/github.js
+++ src/routes/v1/github.js
@@ -5,11 +5,11 @@ const router = express.Router();
 const authenticator = require('../../middlewares/auth');
 const controller = require('../../controllers/v1/github');
 
-router.get('/repositories', authenticator, controller.getRepos);
-router.put('/starred/:owner/:repo', authenticator, controller.starRepo);
-router.delete('/starred/:owner/:repo', authenticator, controller.unstarRepo);
-router.get('/pulls/:owner/:repo', authenticator, controller.getPullRequests);
-router.get('/issues/:owner/:repo', authenticator, controller.getIssues);
-router.get('/starred', authenticator, controller.getStarredRepos);
+router.get('/repositories', authenticator(), controller.getRepos);
+router.put('/starred/:owner/:repo', authenticator(), controller.starRepo);
+router.delete('/starred/:owner/:repo', authenticator(), controller.unstarRepo);
+router.get('/pulls/:owner/:repo', authenticator(), controller.getPullRequests);
+router.get('/issues/:owner/:repo', authenticator(), controller.getIssues);
+router.get('/starred', authenticator(), controller.getStarredRepos);
 
 module.exports = router;
