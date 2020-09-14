59abd3570be7cc569c764fe2f2c3321606f00a63
Dishebh Bhayana <dishebh27@gmail.com>
2020-09-14 17:02:14 +0530

    removed name field from Discussion schema

--- src/controllers/v1/discussion.js
+++ src/controllers/v1/discussion.js
@@ -1,20 +1,16 @@
 const create = require('../create');
 const Discussion = require('../../models/Discussion');
 const validators = require('../../validators/discussion');
-const User = require('../../models/User');
 
 module.exports = {
   postDiscussion: create(
     async (req, res) => {
       const { question, repository } = req.body;
 
-      const user = await User.findById(req.user.id);
-
       const newDiscussion = new Discussion({
         question,
         repository,
         userId: req.user.id,
-        name: user.name,
       });
 
       const discussion = await newDiscussion.save();
