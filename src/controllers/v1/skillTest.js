0ecc9ccd6203fd2febb3d36f79d7ba5dc8ca3241
Dishebh Bhayana <dishebh27@gmail.com>
2020-09-21 12:18:58 +0530

    minor changes in updateKillTestQuestion controller

--- src/controllers/v1/skillTest.js
+++ src/controllers/v1/skillTest.js
@@ -82,19 +82,15 @@ module.exports = {
       const { options, correctIndex } = req.body;
       let optionsLength = options ? options.length : 0;
 
-      if (!optionsLength) {
-        const skillTestQuestion = await SkillTestQuestion.findById(
-          questionId,
-        ).select('options');
+      const skillTestQuestion = await SkillTestQuestion.findById(questionId);
 
+      if (!optionsLength) {
         optionsLength = skillTestQuestion.options.length;
       }
 
       if (correctIndex && (correctIndex < 0 || correctIndex >= optionsLength)) {
         return res.status(400).send('Bad request');
       }
-
-      const skillTestQuestion = await SkillTestQuestion.findById(questionId);
 
       Object.keys(res.locals.inputBody).forEach((key) => {
         skillTestQuestion[key] = res.locals.inputBody[key];
