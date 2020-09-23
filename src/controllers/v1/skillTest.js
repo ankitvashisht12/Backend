6691b8d90b0671a26b05e8abad7763f9b92774f7
Dishebh Bhayana <43489708+Dishebh@users.noreply.github.com>
2020-09-23 13:19:23 +0530

    added route to display all skill test questions (#109)
    * added route to display all skill test questions
    
    * updated getSkillTestQuestions controller
    
    * Update skillTest.js

--- src/controllers/v1/skillTest.js
+++ src/controllers/v1/skillTest.js
@@ -2,8 +2,39 @@ const create = require('../create');
 const SkillTest = require('../../models/SkillTest');
 const SkillTestQuestion = require('../../models/SkillTestQuestion');
 const validators = require('../../validators/skillTest');
+const ROLES = require('../../config/roles');
 
 module.exports = {
+  getSkillTestQuestions: create(async (req, res) => {
+    const { testId } = req.params;
+    const { page = 1, per_page = 10 } = req.query;
+
+    const skillTest = await SkillTest.findById(testId);
+
+    if (!skillTest.isPublished && req.user.role !== ROLES.ADMIN) {
+      return res
+        .status(403)
+        .send('The user is forbidden to access the skill test');
+    }
+
+    const skillTestQuestions = await SkillTestQuestion.find({ testId })
+      // eslint-disable-next-line camelcase
+      .limit(per_page * 1)
+      // eslint-disable-next-line camelcase
+      .skip((page - 1) * per_page);
+
+    const count = await SkillTestQuestion.find({ testId }).countDocuments();
+
+    return res.json({
+      data: {
+        // eslint-disable-next-line camelcase
+        totalPages: Math.ceil(count / per_page),
+        currentPage: page,
+        skillTestQuestions,
+      },
+    });
+  }),
+
   postSkillTest: create(
     async (req, res) => {
       const { name, image, description } = req.body;
@@ -138,6 +169,20 @@ module.exports = {
     await SkillTestQuestion.findByIdAndRemove(questionId);
 
     res.status(200).send('Skill Test Question removed successfully');
+  }),
+
+  publishSkillTest: create(async (req, res) => {
+    const { testId } = req.params;
+
+    await SkillTest.findByIdAndUpdate(
+      testId,
+      {
+        isPublished: true,
+      },
+      { new: true },
+    );
+
+    res.status(200).send('Skill Test published successfully');
   }),
 
   unpublishSkillTest: create(async (req, res) => {
--- src/routes/v1/skillTest.js
+++ src/routes/v1/skillTest.js
@@ -6,6 +6,7 @@ const authenticator = require('../../middlewares/auth');
 const controller = require('../../controllers/v1/skillTest');
 const ROLES = require('../../config/roles');
 
+router.get('/:testId', authenticator(), controller.getSkillTestQuestions);
 router.post('/', authenticator(ROLES.ADMIN), controller.postSkillTest);
 router.post(
   '/publish/:testId',
